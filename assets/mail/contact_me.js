57b8ecb3646f25aa10c196afab46f3c4e1dd0b4c
MapathaMatiba <160006763+MapathaMatiba@users.noreply.github.com>
2024-05-02 21:20:10 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://2fnbleoef4.execute-api.eu-west-1.amazonaws.com/2401FTDE-MATMAP-CC-predict-lambda",
+                url: "https://2fnbleoef4.execute-api.eu-west-1.amazonaws.com/2401FTDE-MATMAP-CC-predict-API-GATEWAY",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
