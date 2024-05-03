927242acfcdc8bf83084e2d071e19d2af3a8143d
MapathaMatiba <160006763+MapathaMatiba@users.noreply.github.com>
2024-05-03 13:31:34 +0200

    Add files via upload

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://7cym1teqf3.execute-api.eu-west-1.amazonaws.com",
+                url: "{Insert your own AWS API Gateway Endpoint URL here}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
