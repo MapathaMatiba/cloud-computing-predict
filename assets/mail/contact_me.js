4ca456fdff1ddcf357ce6e6fe1c137f9d453b16e
MapathaMatiba <160006763+MapathaMatiba@users.noreply.github.com>
2024-05-03 12:48:34 +0200

    Update contact_me.js
    ADDING URL

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://2fnbleoef4.execute-api.eu-west-1.amazonaws.com",
+                url: "https://7cym1teqf3.execute-api.eu-west-1.amazonaws.com",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
